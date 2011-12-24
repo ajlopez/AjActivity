83550d5859a34efdda17b75ea3ce3f786fcdff9b
ajlopez <webmaster@ajlopez.com>
2011-12-24 10:31:55 -0300

    Message with first creation test

---
+++ Src/AjActivity.Tests/Model/MessageTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjActivity.Model;
+
+namespace AjActivity.Tests.Model
+{
+    [TestClass]
+    public class MessageTests
+    {
+        [TestMethod]
+        public void CreateMessage()
+        {
+            DateTime datetime = DateTime.UtcNow;
+            Message message = new Message(1, 2, datetime, "foo");
+
+            Assert.AreEqual(1, message.Id);
+            Assert.AreEqual(2, message.UserId);
+            Assert.AreEqual(datetime, message.DateTime);
+            Assert.AreEqual("foo", message.Content);
+        }
+    }
+}
---
+++ Src/AjActivity/Model/Message.cs
@@ -0,0 +1,32 @@
+﻿namespace AjActivity.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Message
+    {
+        private long id;
+        private long userid;
+        private DateTime datetime;
+        private string content;
+
+        public Message(long id, long userid, DateTime datetime, string content)
+        {
+            this.id = id;
+            this.userid = userid;
+            this.datetime = datetime;
+            this.content = content;
+        }
+
+        public long Id { get { return this.id; } }
+
+        public long UserId { get { return this.userid; } }
+
+        public DateTime DateTime { get { return this.datetime; } }
+
+        public string Content { get { return this.content; } }
+    }
+}
+
