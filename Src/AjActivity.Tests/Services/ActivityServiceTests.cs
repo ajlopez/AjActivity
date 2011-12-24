1e73db5f4432f158dd96160112ef9b80e38ec7bd
ajlopez <webmaster@ajlopez.com>
2011-12-24 11:17:05 -0300

    ActivityService, first test

---
+++ Src/AjActivity.Tests/Services/ActivityServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjActivity.Services;
+
+namespace AjActivity.Tests.Services
+{
+    [TestClass]
+    public class ActivityServiceTests
+    {
+        [TestMethod]
+        public void NewMessage()
+        {
+            ActivityService service = new ActivityService();
+
+            ulong id = service.NewMessage(1, "foo");
+
+            Assert.IsTrue(id > 0);
+        }
+    }
+}
---
+++ Src/AjActivity/Services/ActivityService.cs
@@ -0,0 +1,21 @@
+﻿namespace AjActivity.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjActivity.Model;
+
+    public class ActivityService
+    {
+        private ulong counter;
+
+        public ulong NewMessage(ulong userid, string content)
+        {
+            DateTime datetime = DateTime.UtcNow;
+            ulong id = ++counter;
+            new Message(id, userid, datetime, content);
+            return id;
+        }
+    }
+}
