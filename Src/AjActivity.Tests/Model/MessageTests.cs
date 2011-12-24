d527b1f47157d80b8cb9e2b1698b070f4dc34873
ajlopez <webmaster@ajlopez.com>
2011-12-24 10:36:50 -0300

    Message bad arguments in creation, with tests

--- Src/AjActivity.Tests/Model/MessageTests.cs
+++ Src/AjActivity.Tests/Model/MessageTests.cs
@@ -16,10 +16,34 @@ namespace AjActivity.Tests.Model
             DateTime datetime = DateTime.UtcNow;
             Message message = new Message(1, 2, datetime, "foo");
 
-            Assert.AreEqual(1, message.Id);
-            Assert.AreEqual(2, message.UserId);
+            Assert.AreEqual(1u, message.Id);
+            Assert.AreEqual(2u, message.UserId);
             Assert.AreEqual(datetime, message.DateTime);
             Assert.AreEqual("foo", message.Content);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void RaiseWhenIdIsZero()
+        {
+            DateTime datetime = DateTime.UtcNow;
+            Message message = new Message(0, 2, datetime, "foo");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void RaiseWhenUserIdIsZero()
+        {
+            DateTime datetime = DateTime.UtcNow;
+            Message message = new Message(1, 0, datetime, "foo");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RaiseWhenContentIsNull()
+        {
+            DateTime datetime = DateTime.UtcNow;
+            Message message = new Message(1, 2, datetime, null);
         }
     }
 }
--- Src/AjActivity/Model/Message.cs
+++ Src/AjActivity/Model/Message.cs
@@ -7,22 +7,29 @@
 
     public class Message
     {
-        private long id;
-        private long userid;
+        private ulong id;
+        private ulong userid;
         private DateTime datetime;
         private string content;
 
-        public Message(long id, long userid, DateTime datetime, string content)
+        public Message(ulong id, ulong userid, DateTime datetime, string content)
         {
+            if (id == 0)
+                throw new ArgumentOutOfRangeException("id");
+            if (userid == 0)
+                throw new ArgumentOutOfRangeException("userid");
+            if (string.IsNullOrEmpty(content))
+                throw new ArgumentNullException("content");
+
             this.id = id;
             this.userid = userid;
             this.datetime = datetime;
             this.content = content;
         }
 
-        public long Id { get { return this.id; } }
+        public ulong Id { get { return this.id; } }
 
-        public long UserId { get { return this.userid; } }
+        public ulong UserId { get { return this.userid; } }
 
         public DateTime DateTime { get { return this.datetime; } }
 
