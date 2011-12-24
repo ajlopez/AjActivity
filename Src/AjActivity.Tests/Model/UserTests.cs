d5e1ac71a8da28b079225f528ca611701de7882c
ajlopez <webmaster@ajlopez.com>
2011-12-24 10:49:45 -0300

    User with first tests

---
+++ Src/AjActivity.Tests/Model/UserTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjActivity.Model;
+
+namespace AjActivity.Tests.Model
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void CreateUser()
+        {
+            User user = new User(1, "foo");
+
+            Assert.AreEqual(1u, user.Id);
+            Assert.AreEqual("foo", user.Name);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void RaiseWhenIdIsZero()
+        {
+            User user = new User(0, "foo");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RaiseWhenNameIsNull()
+        {
+            User user = new User(1, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RaiseWhenNameIsEmpty()
+        {
+            User user = new User(1, "");
+        }
+    }
+}
---
+++ Src/AjActivity/Model/User.cs
@@ -0,0 +1,30 @@
+﻿namespace AjActivity.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class User
+    {
+        private ulong id;
+        private string name;
+
+        public User(ulong id, string name)
+        {
+            if (id == 0)
+                throw new ArgumentOutOfRangeException("id");
+
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("name");
+
+            this.id = id;
+            this.name = name;
+        }
+
+        public ulong Id { get { return this.id; } }
+
+        public string Name { get { return this.name; } }
+    }
+}
+
