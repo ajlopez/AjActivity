3d649724196229e43a0b15f3a45c146a72d35eba
ajlopez <webmaster@ajlopez.com>
2011-12-25 15:45:00 -0300

    TreeList with first test

---
+++ Src/AjActivity.Tests/Collections/TreeListTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjActivity.Collections;
+
+namespace AjActivity.Tests.Collections
+{
+    [TestClass]
+    public class TreeListTests
+    {
+        [TestMethod]
+        public void AddOneElement()
+        {
+            TreeList<int> list = new TreeList<int>();
+            list.Add(1);
+            Assert.AreEqual(1, list.Count);
+        }
+    }
+}
---
+++ Src/AjActivity/Collections/TreeList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AjActivity.Collections
+{
+    public class TreeList<T>
+    {
+        private long count;
+
+        public void Add(T element)
+        {
+            count++;
+        }
+
+        public long Count { get { return this.count; } }
+    }
+}
